452a890f662b88e7c24fd372b3146cdddb2419e6
cassidy1 <rcassidy@hashicorp.com>
2019-05-21 08:52:42 -0700

    adding tags

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "public_dns" {
-  value = "${aws_instance.ubuntu.public_dns}"
+output "instance-id" {
+  description = "The EC2 instance ID"
+  value       = "${aws_instance.instance.id}"
+}
+
+output "instance-public-dns" {
+  description = "The EC2 instance public DNS"
+  value       = "${aws_instance.instance.public_dns}"
 }
