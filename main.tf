f1e017f2b7e587e0dd7df3ffc524d8c539e7c8c8
NAME <ryancassidy0@gmail.com>
2020-07-21 12:53:45 -0700

    0.12upgrade

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
 terraform {
-  required_version = ">= 0.11.1"
+  required_version = ">= 0.12"
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}b"
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  availability_zone           = "${var.aws_region}b"
   associate_public_ip_address = "true"
-  subnet_id              = var.subnet_ids[0]
-  tags {
-    Name = "${var.name}"
-    owner = "${var.owner}"
-    ttl = "24"
+  subnet_id                   = var.subnet_ids[0]
+  tags = {
+    Name  = var.name
+    owner = var.owner
+    ttl   = "24"
   }
 }
+
--- variables.tf
+++ variables.tf
@@ -1,28 +1,29 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-1"
+  default     = "us-east-1"
 }
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default     = "ami-2e1ef954"
 }
 
 variable "instance_type" {
   description = "type of EC2 instance to provision. (t2.micro, t2.small, t2.medium, t2.large, etc.)"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "name" {
   description = "name to pass to Name tag"
-  default = "tfe-demo-1"
+  default     = "tfe-demo-1"
 }
 
 variable "owner" {
   description = "identifier of who is provisioning"
-  default = "rcassidy"
+  default     = "rcassidy"
 }
 
 variable "subnet_ids" {
-  description = "To use a predefined subnet use ${data.[subnet]"
-} 
+  description = "To use a predefined subnet use $${data.[subnet]}"
+}
+
